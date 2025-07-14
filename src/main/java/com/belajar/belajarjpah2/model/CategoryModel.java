de55144d4efc987888ceadf9e1f565e646f5f5ad
Leryv <andikagamers50@gmail.com>
2025-07-14 14:14:36 +0700

    update

--- src/main/java/com/belajar/belajarjpah2/model/CategoryModel.java
+++ src/main/java/com/belajar/belajarjpah2/model/CategoryModel.java
@@ -2,10 +2,12 @@ package com.belajar.belajarjpah2.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "categories")
 public class CategoryModel {
     @Id
     private Long categoryId;
